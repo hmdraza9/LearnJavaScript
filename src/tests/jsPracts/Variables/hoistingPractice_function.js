914d7bbfe87f330a0327610b94df50d8fdd018f1
Hamid Raza <hmdraza9@gmail.com>
2024-12-18 14:37:53 +0530

    practiced hoisting var and function

---
+++ src/tests/jsPracts/Variables/hoistingPractice_function.js
@@ -0,0 +1,29 @@
+
+greet();
+
+function greet(){
+    console.log("Hello world_1!");
+}
+greet();
+
+
+function greet(){
+    console.log("Hello world!");
+}
+
+
+greet();
+
+
+const greet1 = function (){
+    console.log("Hello 1");
+};
+
+
+const greet2 = function (){
+    console.log("Hello 2");
+};
+
+greet1();
+
+greet2();
---
+++ src/tests/jsPracts/Variables/hoistingPractice_variables.js
@@ -0,0 +1,15 @@
+console.log(`c: ${c}`);
+
+console.log(`1x: ${x}`);
+
+var x;
+
+console.log(`2x: ${x}`);
+
+x = 5;
+console.log(`3x: ${x}`);
+x = 45;
+
+console.log(`4x: ${x}`);
+
+var c;
